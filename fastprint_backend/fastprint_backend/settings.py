d5d667f64e97a51e539652b6b574e635868efc1e
AyanAhmed30 <hafizayanahmed9@gmail.com>
2025-06-24 02:49:42 +0500

    Removed email password to fix GitHub push block

--- fastprint_backend/fastprint_backend/settings.py
+++ fastprint_backend/fastprint_backend/settings.py
@@ -146,5 +146,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ayan3092003@gmail.com'
-EMAIL_HOST_PASSWORD = 'uqcy qpda lxyr zycw'  # <-- your app password
+# EMAIL_HOST_PASSWORD = 'uqcy qpda lxyr zycw'  # <-- your app password
 DEFAULT_FROM_EMAIL = 'FastPrintGuys <your_email@gmail.com>'
